67a95c69685ff3c7a9f357a08ee41e209b7d682a
sylwuskak <sylwuskak@gmail.com>
2020-01-11 13:02:36 +0100

    invalid languages error fix

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
+    I18n.locale = (['en', 'pl'].include?(params[:locale]) ? params[:locale] : nil ) || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
   end
 
   def default_url_options
