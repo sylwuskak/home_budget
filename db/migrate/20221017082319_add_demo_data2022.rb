3be83763ed911480471ae223fbe349e64c706c6a
Sylwia Poterała <sylwia.poterala@gmail.com>
2022-10-17 10:25:02 +0200

    demo data

---
+++ db/migrate/20221017082319_add_demo_data2022.rb
@@ -0,0 +1,36 @@
+class AddDemoData2022 < ActiveRecord::Migration[5.0]
+  def change
+
+    user = User.where(email: "demo@example.com").first
+
+    (Date.new(2022, 1, 1)..Date.new(2022, 12, 31)).each do |d|
+      Expense.create!(
+        user: user, 
+        date: d, 
+        category: user.categories.select{|c| c.category_type == 'Expense'}.sample,
+        amount: (1..100).to_a.sample,
+        description: ''
+      )
+      if d.day == 1 
+        Incoming.create!(
+          user: user, 
+          date: d, 
+          category: user.categories.select{|c| c.category_type == 'Incoming'}[1],
+          amount: 2500,
+          description: ''
+        )
+
+        user.categories.select{|c| c.category_type == 'Expense'}.each do |c|
+          Budget.create!(
+            user: user,
+            date: d, 
+            category: c,
+            amount: (50..1000).to_a.sample
+          )
+        end
+        
+      end
+
+    end
+  end
+end
