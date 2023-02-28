95bd65e03284d966881580244b788102502118bf
Sylwia Poterala <sylwia.poterala@gmail.com>
2023-02-28 13:48:31 +0100

    rails 6.1

--- Gemfile
+++ Gemfile
@@ -2,24 +2,24 @@ source 'https://rubygems.org'
 ruby '2.7.4'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
-gem 'rails', '~> 5.2', '>= 5.2.8.1'
+gem 'rails', '~> 6.1', '>= 6.1.7.2'
 
 # Use sqlite3 as the database for Active Record
 # Use Puma as the app server
-gem 'puma', '~> 3.0'
+gem 'puma', '~> 6.1', '>= 6.1.1'
 # Use SCSS for stylesheets
-gem 'sass-rails', '~> 5.0'
+gem 'sass-rails', '~> 6.0'
 # Use Uglifier as compressor for JavaScript assets
-gem 'uglifier', '>= 1.3.0'
+gem 'uglifier', '~> 4.2'
 # Use CoffeeScript for .coffee assets and views
-gem 'coffee-rails', '~> 4.2'
+gem 'coffee-rails', '~> 5.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
-gem 'turbolinks', '~> 5'
+gem 'turbolinks', '~> 5.2', '>= 5.2.1'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
@@ -43,33 +43,34 @@ group :development do
   gem 'spring-watcher-listen', '~> 2.0.0'
 end
 
-gem 'slim', '~> 3.0.7'
-gem 'slim-rails', '~> 3.1.1'
-gem 'bootstrap-sass', '~> 3.3.7'
+gem 'slim', '~> 5.0'
+gem 'slim-rails', :git => 'git@github.com:spoterala/slim-rails.git'
 
-gem 'active_link_to', '~> 1.0.3'
+gem 'bootstrap-sass', '~> 3.4', '>= 3.4.1'
+
+gem 'active_link_to', '~> 1.0', '>= 1.0.5'
 gem 'http_accept_language'
 
-gem 'devise', '~> 4.4.3'
-gem 'bootstrap-datepicker-rails', '~> 1.6', '>= 1.6.1.1'
-gem 'nokogiri', '~> 1.6', '>= 1.6.6.2'
-gem 'bootstrap-table-rails', '~> 1.12', '>= 1.12.1'
-gem 'gruff', '~> 0.7.0'
+gem 'devise', '~> 4.9'
+gem 'bootstrap-datepicker-rails', '~> 1.9', '>= 1.9.0.1'
+gem 'nokogiri', '~> 1.14', '>= 1.14.2'
+gem 'bootstrap-table-rails', '~> 1.20', '>= 1.20.2'
+gem 'gruff', '~> 0.19.0'
 gem 'simple_calendar', '~> 2.4', '>= 2.4.3'
 
-gem 'will_paginate', '~> 3.1', '>= 3.1.6'
-gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.1'
+gem 'will_paginate', '~> 3.3', '>= 3.3.1'
+gem 'will_paginate-bootstrap', '~> 1.0', '>= 1.0.2'
 
-gem 'rspec', '~> 3.0'
+gem 'rspec', '~> 3.12'
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 group :development, :test do
-  gem 'pg', '0.18.2'
+  gem 'pg', '~> 1.4', '>= 1.4.6'
   gem 'rails_12factor', '0.0.3'
 end
 
 group :production do
-  gem 'pg', '0.18.2'
+  gem 'pg', '~> 1.4', '>= 1.4.6'
   gem 'rails_12factor', '0.0.3'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -2,64 +2,64 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `bin/rails
+# db:schema:load`. When creating a new database, `bin/rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221017082319) do
+ActiveRecord::Schema.define(version: 2023_02_28_123628) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "budgets", force: :cascade do |t|
-    t.float   "amount"
-    t.date    "date"
+  create_table "budgets", id: :serial, force: :cascade do |t|
+    t.float "amount"
+    t.date "date"
     t.integer "category_id"
-    t.integer "user_id",     null: false
-    t.index ["category_id"], name: "index_budgets_on_category_id", using: :btree
-    t.index ["user_id"], name: "index_budgets_on_user_id", using: :btree
+    t.integer "user_id", null: false
+    t.index ["category_id"], name: "index_budgets_on_category_id"
+    t.index ["user_id"], name: "index_budgets_on_user_id"
   end
 
-  create_table "categories", force: :cascade do |t|
-    t.string  "category_name"
+  create_table "categories", id: :serial, force: :cascade do |t|
+    t.string "category_name"
     t.integer "user_id"
-    t.string  "category_type",                null: false
-    t.boolean "available",     default: true, null: false
-    t.index ["user_id"], name: "index_categories_on_user_id", using: :btree
+    t.string "category_type", null: false
+    t.boolean "available", default: true, null: false
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
-  create_table "operations", force: :cascade do |t|
-    t.string   "type",                                         null: false
-    t.float    "amount"
-    t.text     "description"
-    t.date     "date"
-    t.integer  "category_id"
-    t.integer  "user_id",                                      null: false
-    t.string   "expense_type"
-    t.datetime "created_at",   default: '2023-02-28 11:38:32', null: false
-    t.index ["category_id"], name: "index_operations_on_category_id", using: :btree
-    t.index ["user_id"], name: "index_operations_on_user_id", using: :btree
+  create_table "operations", id: :serial, force: :cascade do |t|
+    t.string "type", null: false
+    t.float "amount"
+    t.text "description"
+    t.date "date"
+    t.integer "category_id"
+    t.integer "user_id", null: false
+    t.string "expense_type"
+    t.datetime "created_at", default: "2023-02-28 11:38:32", null: false
+    t.index ["category_id"], name: "index_operations_on_category_id"
+    t.index ["user_id"], name: "index_operations_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
+    t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
-    t.string   "current_sign_in_ip"
-    t.string   "last_sign_in_ip"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+    t.string "current_sign_in_ip"
+    t.string "last_sign_in_ip"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "categories", "users"
